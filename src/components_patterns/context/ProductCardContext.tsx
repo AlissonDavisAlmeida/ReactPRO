b62ddc4fe3298b6aa6cb902dc9ef61356a130191
alisson <alissoneatena1@gmail.com>
2022-07-04 14:37:40 -0300

    new section formik

--- src/components_patterns/context/ProductCardContext.tsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react"
-import { ProductContextInterface } from "../interfaces/ProductInterface"
-
-export const ProductContext = createContext<ProductContextInterface>({})
-export const { Provider } = ProductContext
