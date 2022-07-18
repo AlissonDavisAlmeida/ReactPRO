1ee7b1b529dcf6ec19abfbf019a7086711a15177
alisson <alissoneatena1@gmail.com>
2022-07-18 11:26:17 -0300

    finally dynamic forms

--- src/forms/components/MySelectInput.tsx
+++ src/forms/components/MySelectInput.tsx
@@ -3,10 +3,11 @@ import { ErrorMessage, useField } from "formik";
 interface MySelectInputProps {
     label: string;
     name: string;
+    children: React.ReactNode;
     [key: string]: any;
 }
 
-export const MySelectInput= ({ label, ...props }: MySelectInputProps)=> {
+export const MySelectInput = ({ label, children, ...props }: MySelectInputProps) => {
 
     const [field] = useField(props)
 
@@ -14,10 +15,10 @@ export const MySelectInput= ({ label, ...props }: MySelectInputProps)=> {
         <>
             <label htmlFor={props.id || props.name}>{label}</label>
             <select {...field} {...props}>
-                
+                {children}
 
             </select>
-            <ErrorMessage name={props.name} component="span"/>
+            <ErrorMessage name={props.name} component="span" />
         </>
     );
 }
--- src/forms/pages/DynamicForm.tsx
+++ src/forms/pages/DynamicForm.tsx
@@ -1,13 +1,26 @@
 import { Form, Formik } from "formik"
-import { MyTextInput } from "../components"
+import { MySelectInput, MyTextInput } from "../components"
 import formJSON from "../data/custom-form.json"
+import * as Yup from "yup"
 
 export const DynamicForm = () => {
 
     const initialValues: { [key: string]: any } = {}
+    const requiredFields: { [key: string]: any } = {}
 
     formJSON.map(field => {
         initialValues[field.name] = field.value
+
+        if (field.validations) {
+            let schema = Yup.string()
+            field.validations.map(validation => {
+                if (validation.type === "required") {
+                    schema = schema.required(validation.message)
+                }
+            })
+
+            requiredFields[field.name] = schema
+        }
     })
 
     return (
@@ -19,19 +32,42 @@ export const DynamicForm = () => {
                 onSubmit={(values) => {
                     console.log(values)
                 }}
+                validationSchema={Yup.object({
+                    ...requiredFields
+                })}
             >
                 {(formik) => (
 
 
                     <Form>
                         {formJSON.map((field, index) => {
-                            return (
-                                <MyTextInput
-                                    key={index}
-                                    label={field.label}
-                                    name={field.name}
-                                    type={field.type as any} />
-                            )
+
+                            switch (field.type) {
+                                case "input":
+                                    return <MyTextInput
+                                        key={index}
+                                        label={field.label}
+                                        name={field.name}
+                                        type={field.type as any} />
+                                case "email":
+                                    return <MyTextInput
+                                        key={index}
+                                        label={field.label}
+                                        name={field.name}
+                                        type={field.type as any} />
+                                case "select":
+                                    return <MySelectInput
+                                        key={index}
+                                        label={field.label}
+                                        name={field.name}
+                                    >
+                                        {field.options!.map((option, index) => {
+                                            return <option key={option.id} value={option.label}>{option.label}</option>
+                                        })
+                                        }
+                                    </MySelectInput>
+                            }
+
                         })}
                         <button>Submit</button>
                     </Form>
